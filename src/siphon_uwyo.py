ca78e730f37da1157e0c7162f69c303479cb8b0a
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2023-03-20 20:41:09 -0700

    get goa, pune, thiru soundings

---
+++ src/siphon_uwyo.py
@@ -0,0 +1,288 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# To make a kernel with a siphon virtualenv from the terminal:
+#
+# ```
+# conda create -n siphon siphon
+# conda activate siphon
+# conda install ipykernel
+# python -m ipykernel install --user --name=siphon
+# ```
+#
+# Then select the `siphon` from the pulldown/window in Jupyter.
+
+# %%
+from siphon.simplewebservice.wyoming import WyomingUpperAir
+from datetime import datetime
+import os
+import pandas
+
+start_time = datetime(2023, 3, 1, 0)
+end_time = datetime(2023, 6, 30, 0)
+
+station = "43371" # Trivandrum, Thiruvananthapuram, 'VOTX'?
+
+df = WyomingUpperAir.request_data(start_time, station) # returns Pandas dataframe
+
+# %%
+df
+
+# %%
+# for column in df:
+#     print( df[column].name )
+#     print( df[column].values )
+
+# df[column].shape[0]
+df[df.columns[0:8]]
+
+# skip column 8
+
+# scalars
+
+
+df.columns[9]
+type(df[df.columns[9]]) is pandas.core.series.Series
+
+df[df.columns[10]][0].strftime('%Y-%m-%d %H:%M:%S') # convert Datestamp to string
+type(df[df.columns[10]][0]) is pandas._libs.tslibs.timestamps.Timestamp
+    
+
+# %%
+from netCDF4 import Dataset
+
+"convert Timestamps into string, otherwise just return input."
+def Timestamp2String( t ):
+    return ( t.strftime('%Y-%m-%d %H:%M:%S') if type(t) is pandas._libs.tslibs.timestamps.Timestamp else t )
+
+"write a sounding dataframe as a NetCDF4 file."
+def sounding2nc( df, filename ):
+
+    # Open a new NetCDF file for writing
+    nc_file = Dataset(filename, mode='w')
+
+    # Set the attributes from df cols 9...
+    for column in df.columns[9:]:
+        #  print( "set attribute "+df[column].name+"=", Timestamp2String(df[column][0]) )
+        nc_file.setncattr( df[column].name, Timestamp2String(df[column][0]) )
+
+    # or set all at once from a dict: setncatts(self,attdict)
+
+    # Define dimensions for pressure, temperature, etc. variables
+    time_dim = nc_file.createDimension('time', 1)
+    level_dim = nc_file.createDimension('level', df[df.columns[0]].shape[0])
+
+    units_list = [ 'hPa', 'm', 'degrees C', 'degrees C', 'degrees', 'm/s', 'm/s', 'm/s' ]
+
+    # Create variables for time, level, latitude, longitude, pressure, and temperature
+    for i, column in enumerate( df.columns[0:7] ):
+        # create the netcdf variable
+        var = nc_file.createVariable(df[column].name, 'f4', ('time', 'level'))
+        # write the data to the NetCDF file
+        var[:] = df[column][:]
+        # supply units attributes
+        var.units = units_list[i]
+
+    # Close the NetCDF file
+    return nc_file.close()
+
+
+# %%
+"write a sounding dataframe as a NetCDF4 file."
+def sounding2nc( df, filename ):
+
+    # Open a new NetCDF file for writing
+    nc_file = Dataset(filename, mode='w')
+
+    # Set the attributes from df cols 9...
+    for column in df.columns[9:]:
+        #  print( "set attribute "+df[column].name+"=", Timestamp2String(df[column][0]) )
+        nc_file.setncattr( df[column].name, Timestamp2String(df[column][0]) )
+
+    # or set all at once from a dict: setncatts(self,attdict)
+
+    # Define dimensions for pressure, temperature, etc. variables
+    time_dim = nc_file.createDimension('time', 1)
+    level_dim = nc_file.createDimension('level', df[df.columns[0]].shape[0])
+
+    units_list = [ 'hPa', 'm', 'degrees C', 'degrees C', 'degrees', 'm/s', 'm/s', 'm/s' ]
+
+    # Create variables for time, level, latitude, longitude, pressure, and temperature
+    for i, column in enumerate( df.columns[0:7] ):
+        # create the netcdf variable
+        var = nc_file.createVariable(df[column].name, 'f4', ('time', 'level'))
+        # write the data to the NetCDF file
+        var[:] = df[column][:]
+        # supply units attributes
+        var.units = units_list[i]
+
+    # Close the NetCDF file
+    return nc_file.close()
+
+
+# %%
+# get Trivandrum soundings from 2019, write to netcdf
+
+start_time = datetime(2019, 3, 1, 0)
+end_time = datetime(2019, 6, 30, 0)
+station = "43371" # Trivandrum, Thiruvananthapuram, 'VOTX'?
+
+for dt in pandas.date_range(start_time, end_time, freq='12H'):
+    try:
+        df = WyomingUpperAir.request_data(dt, station) # returns Pandas dataframe
+        sounding2nc( df, "../data/uwyo/trivandrum/Trivandrum"+df.time[0].strftime('%Y%m%d_%H%M')+".nc" )
+    except:
+        continue
+    else:
+        continue
+        
+
+
+
+# %%
+start_time = datetime(2019, 3, 1, 0)
+end_time = datetime(2019, 6, 30, 0)
+station = "43063"  # Pune
+for dt in pandas.date_range(start_time, end_time, freq='12H'):
+    try:
+        df = WyomingUpperAir.request_data(dt, station) # returns Pandas dataframe
+        sounding2nc( df, "../data/uwyo/pune/Pune"+df.time[0].strftime('%Y%m%d_%H%M')+".nc" )
+    except:
+        continue
+    finally:
+        continue
+
+# %%
+# IGRA2
+from siphon.simplewebservice.igra2 import IGRAUpperAir
+
+station = "INM00043192" # Goa
+df, header = IGRAUpperAir.request_data(start_time, station)
+# # ! probably in a different format!
+
+# %%
+type(df.columns) # pandas.core.indexes.base.Index
+# var_indx = pandas.core.indexes.base.Index(['pressure', 'height', 'temperature', 'dewpoint'])
+header
+df
+units_list = [ 'none', 'none', 'seconds?', 'hPa', 'none', 'meters', 'none', 'degrees C', 'none', 'percent', 'degrees', 'm/s', 'datestring', 'm/s', 'm/s', 'degree C']
+    
+# df.drop(columns='date')
+# for i, column in enumerate(df.columns):
+#     print( i, column, units_list[i] )
+
+dfd = df.drop(columns='date')
+for i, column in enumerate(dfd.columns):
+    print( i, column, units_list[i] )
+
+# %%
+"write an IGRA2 sounding dataframe as a NetCDF4 file."
+def igra2nc( df, header, filename ):
+
+    # Open a new NetCDF file for writing
+    nc_file = Dataset(filename, mode='w')
+
+    # Set the attributes from header
+    for column in header.columns:
+        nc_file.setncattr( header[column].name, Timestamp2String(header[column][0]) )
+
+    # or set all at once from a dict: setncatts(self,attdict)
+
+    # Define dimensions for pressure, temperature, etc. variables
+    time_dim = nc_file.createDimension('time', 1)
+    level_dim = nc_file.createDimension('level', df[df.columns[0]].shape[0])
+
+    dfd = df.drop(columns='date') # Datestamp redundant and type not allowed in NetCDF
+    units_list = [ 'none', 'none', 'seconds?', 'hPa', 'none', 'meters', 'none', 'degrees C', 'none', 'percent', 'degrees', 'm/s', #'datestring', 
+                   'm/s', 'm/s', 'degree C']
+
+    # Create variables for time, level, latitude, longitude, pressure, and temperature
+    for i, column in enumerate( dfd.columns ):
+        # create the netcdf variable
+        var = nc_file.createVariable(dfd[column].name, 'f4', ('time', 'level'))
+        # write the data to the NetCDF file
+        var[:] = dfd[column][:]
+        # supply units attributes
+        var.units = units_list[i]
+
+    # Close the NetCDF file
+    return nc_file.close()
+
+
+# %%
+pandas.date_range(start_time, end_time, freq='D')
+
+# %%
+start_time = datetime(2019, 3, 1, 0)
+end_time = datetime(2019, 6, 30, 0)
+station = "INM00043192" # Goa
+
+
+for dt in pandas.date_range(start_time, end_time, freq='D'): # freq='12H'
+    my_path = "../data/igra2/goa/Goa"+dt.strftime('%Y%m%d_%H%M')+".nc"
+    if not os.path.exists(my_path) or os.path.getsize(my_path) <= 0:
+        try:
+            df, header = IGRAUpperAir.request_data(dt.to_pydatetime(), station)
+            igra2nc( df, header, my_path )
+        except:
+            # print exception!
+            continue
+        finally:
+            continue
+    else:
+        continue
+
+        
+# IGRA2 downloading is slow.
+# There does not appear to be any 12Z soundings.
+
+
+# %%
+def get_igra2_station( daterange, station, stationname ):
+    for dt in daterange: # freq='12H'
+        my_path = "../data/igra2/"+stationname+"/"+stationname+dt.strftime('%Y%m%d_%H%M')+".nc"
+        if not os.path.exists(my_path) or os.path.getsize(my_path) <= 0:
+            try:
+                df, header = IGRAUpperAir.request_data(dt.to_pydatetime(), station)
+                igra2nc( df, header, my_path )
+            except:
+                # print exception!
+                continue
+            finally:
+                continue
+        else:
+            continue
+
+
+# %%
+stationname = 'pune'
+station = 'INM00043063'
+start_time = datetime(2019, 3, 1, 0)
+end_time = datetime(2019, 6, 30, 0)
+dr = pandas.date_range(start_time, end_time, freq='D')
+
+get_igra2_station( dr, station, stationname )
+
+# %%
+stationname = 'thrivandrum'
+station = 'INM00043371'
+start_time = datetime(2019, 3, 1, 0)
+end_time = datetime(2019, 6, 30, 0)
+dr = pandas.date_range(start_time, end_time, freq='D')
+
+get_igra2_station( dr, station, stationname )
+
+# %%
