bc3589f4ee946357aa397566d503f4f73a8785ff
Simon de Szoeke <simon.deszoeke@oregonstate.edu>
2023-03-22 17:31:38 -0700

    load 2018-2022, plot Thiru 2018-2022

--- src/siphon_uwyo.py
+++ src/siphon_uwyo.py
@@ -66,46 +66,13 @@ type(df[df.columns[9]]) is pandas.core.series.Series
 
 df[df.columns[10]][0].strftime('%Y-%m-%d %H:%M:%S') # convert Datestamp to string
 type(df[df.columns[10]][0]) is pandas._libs.tslibs.timestamps.Timestamp
-    
+
 
 # %%
 "convert Timestamps into string, otherwise just return input."
 def Timestamp2String( t ):
     return ( t.strftime('%Y-%m-%d %H:%M:%S') if type(t) is pandas._libs.tslibs.timestamps.Timestamp else t )
 
-"write a sounding dataframe as a NetCDF4 file."
-def sounding2nc( df, filename ):
-
-    # Open a new NetCDF file for writing
-    nc_file = Dataset(filename, mode='w')
-
-    # Set the attributes from df cols 9...
-    for column in df.columns[9:]:
-        #  print( "set attribute "+df[column].name+"=", Timestamp2String(df[column][0]) )
-        nc_file.setncattr( df[column].name, Timestamp2String(df[column][0]) )
-
-    # or set all at once from a dict: setncatts(self,attdict)
-
-    # Define dimensions for pressure, temperature, etc. variables
-    time_dim = nc_file.createDimension('time', 1)
-    level_dim = nc_file.createDimension('level', df[df.columns[0]].shape[0])
-
-    units_list = [ 'hPa', 'm', 'degrees C', 'degrees C', 'degrees', 'm/s', 'm/s', 'm/s' ]
-
-    # Create variables for time, level, latitude, longitude, pressure, and temperature
-    for i, column in enumerate( df.columns[0:7] ):
-        # create the netcdf variable
-        var = nc_file.createVariable(df[column].name, 'f4', ('time', 'level'))
-        # write the data to the NetCDF file
-        var[:] = df[column][:]
-        # supply units attributes
-        var.units = units_list[i]
-
-    # Close the NetCDF file
-    return nc_file.close()
-
-
-# %%
 "write a sounding dataframe as a NetCDF4 file."
 def sounding2nc( df, filename ):
 
@@ -218,33 +185,6 @@ def igra2nc( df, header, filename ):
     # Close the NetCDF file
     return nc_file.close()
 
-
-# %%
-start_time = datetime(2019, 3, 1, 0)
-end_time = datetime(2019, 6, 30, 0)
-station = "INM00043192" # Goa
-
-
-for dt in pandas.date_range(start_time, end_time, freq='D'): # freq='12H'
-    my_path = "../data/igra2/goa/goa"+dt.strftime('%Y%m%d_%H%M')+".nc"
-    if not os.path.exists(my_path) or os.path.getsize(my_path) <= 0:
-        try:
-            df, header = IGRAUpperAir.request_data(dt.to_pydatetime(), station)
-            igra2nc( df, header, my_path )
-        except:
-            # print exception!
-            continue
-        finally:
-            continue
-    else:
-        continue
-
-        
-# IGRA2 downloading is slow.
-# There does not appear to be any 12Z soundings.
-
-
-# %%
 def get_igra2_station( daterange, station, stationname ):
     for dt in daterange: # freq='12H'
         my_path = "../data/igra2/"+stationname+"/"+stationname+dt.strftime('%Y%m%d_%H%M')+".nc"
@@ -262,48 +202,21 @@ def get_igra2_station( daterange, station, stationname ):
 
 
 # %%
+# test get_igra2_station
+
 stationname = 'pune'
 station = 'INM00043063'
 start_time = datetime(2019, 3, 1, 0)
 end_time = datetime(2019, 6, 30, 0)
 dr = pandas.date_range(start_time, end_time, freq='D')
-
 get_igra2_station( dr, station, stationname )
 
-# %%
 stationname = 'trivandrum'
 station = 'INM00043371'
 start_time = datetime(2019, 3, 1, 0)
 end_time = datetime(2019, 6, 30, 0)
 dr = pandas.date_range(start_time, end_time, freq='D')
-
 get_igra2_station( dr, station, stationname )
-
-# %%
-stationname = 'pune'
-station = 'INM00043063'
-start_time = datetime(2018, 3, 1, 0)
-end_time = datetime(2018, 6, 30, 0)
-dr = pandas.date_range(start_time, end_time, freq='D')
-
-get_igra2_station( dr, station, stationname )
-
-# %%
-stationname = 'trivandrum'
-station = 'INM00043371'
-start_time = datetime(2018, 3, 1, 0)
-end_time = datetime(2018, 6, 30, 0)
-dr = pandas.date_range(start_time, end_time, freq='D')
-
-get_igra2_station( dr, station, stationname )
-
-# %%
-start_time = datetime(year, 3, 1, 0)
-end_time = datetime(year, 6, 30, 0)
-
-for year in [2020, 2021, 2022]:
-    get_igra2_station( pandas.date_range(start_time, end_time, freq='D'), 'INM00043371', 'trivandrum'  )
-    get_igra2_station( pandas.date_range(start_time, end_time, freq='D'), 'INM00043063', 'pune'  )
 
 # %%
 # batch rename Trivandrum files
@@ -318,11 +231,19 @@ for filename in os.listdir("../data/igra2/goa/"):
         os.rename("../data/igra2/goa/"+filename, "../data/igra2/goa/"+"goa"+filename[4:])
 
 # %%
-station = "INM00043192" # Goa
+# load all the data for all stations
 
+# list of all stations (lists and tuples) -- try to be pythonic!
+X = [ ("INM00043192", "goa"), ("INM00043371", "trivandrum"), ("INM00043063", "pune") ]
+stationid = [ x[0] for x in X ]
+stationname = [ x[1] for x in X ]
+station_dict = dict( zip(stationname, stationid) )
+# station_dict.keys()
+
+# load all the data for all stations
 for year in [2018, 2019, 2020, 2021, 2022]:
-    start_time = datetime(year, 3, 1, 0)
-    end_time = datetime(year, 6, 30, 0)
-    get_igra2_station( pandas.date_range(start_time, end_time, freq='D'), station, 'goa' )
+    dr = pandas.date_range(datetime(year, 3, 1, 0), datetime(year, 6, 30, 0), freq='D')
+    for stationname in station_dict.keys():
+        get_igra2_station( dr, station_dict[stationname], stationname )
 
 # %%
