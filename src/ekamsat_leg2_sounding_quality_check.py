2ac3bea03fdfe20369a18f1bf62287870f0982dc
Jayesh Phadtare <61420165+jayphd@users.noreply.github.com>
2024-08-13 18:06:25 -0400

    Add files via upload
    Created on Fri May 10 10:13:20 2024
    The script currently checks for sounding dataset (.nc files) for the following errors: 1. non-numeric entry, 
    2. quantities exceeding physical limits, 3. height not increasing monotonically, 4. fast ascent.
    
    Ref: Loehrer, S. M., Edmands, T. A., & Moore, J. A. (1996). TOGA COARE upper-air sounding data archive: Development and quality control procedures. 
    Bulletin of the American Meteorological Society, 77(11), 2651-2672.
    
    It also plots the soundings on thephigrams and calculates instability parameters: CAPE, CIN,TPW, LCL,LFC, Mixed layer height..
    @author: jayesh.phadtare@gmail.com

--- src/ekamsat_leg2_sounding_quality_check.py
+++ src/ekamsat_leg2_sounding_quality_check.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 10 10:13:20 2024
-The script currently checks the sounding dataset (.nc files) for the following errors:
-1. non-numeric entry, 
-2. quantities exceeding physical limits,
-3. height not increasing monotonically.
-4. Ascent rate exceeding 20 m/s 
+The scripts currently checks for sounding dataset (.nc files) for the following errors: 1. non-numeric entry, 
+2. quantities exceeding physical limits, 3. height not increasing monotonically. 
 
-The erroneous entries are replaced by NaNs and the new soundings are stored in .csv format.
+Ref: Loehrer, S. M., Edmands, T. A., & Moore, J. A. (1996). TOGA COARE upper-air sounding data archive: Development and quality control procedures. 
+Bulletin of the American Meteorological Society, 77(11), 2651-2672.
 
-Ref: Loehrer, S. M., Edmands, T. A., & Moore, J. A. (1996). TOGA COARE upper-air sounding data archive: Development and quality control procedures. Bulletin of the American Meteorological Society, 77(11), 2651-2672.
-
-It also plots the soundings on thephigrams and calculates instability parameters: CAPE, CIN,TPW, LCL,LFC, Mixed layer height.
-
+It also plots the soundings on thephigrams and calculates instability parameters: CAPE, CIN,TPW, LCL,LFC, Mixed layer height..
 @author: jayes
 """
 
@@ -35,7 +30,6 @@ file_names =[]
 num_errors = []
 lim_errors = []
 ord_errors = []
-asc_errors = []
 capes = []
 cins  = []
 pws   = []
@@ -63,28 +57,24 @@ for f_name in file_list:
         tm = ncid["time"][:]
         Lat = ncid["lats"][:]
         Lon = ncid["lons"][:]
-        asc = ncid["asc"][:]
         
-        start_h = 0 # starting index
-        data = np.array([height[start_h:], P[start_h:], ws[start_h:], wd[start_h:], T[start_h:], Td[start_h:], rh[start_h:], asc[start_h:]])
+        start_h = 3
+        data = np.array([height[start_h:], P[start_h:], ws[start_h:], wd[start_h:], T[start_h:], Td[start_h:], rh[start_h:]])
         df_data   = pd.DataFrame(np.transpose(data), columns = ['Height (m)','Pressure (hPa)', 'Speed (m/s)', 'Direction (deg)', 'Temperature (C)', 
-                                                  'Dewpoint (C)', 'RH (%)', 'Ascent (m/min)'])
+                                                  'Dewpoint (C)', 'RH (%)'])
         
         # Plot soundings on a tephigram
         sqf.plot_profile(df_data, f_name, path_in, Lat, Lon)        
 
     
+
         # get error report and instability paramters
-        [num_error, lim_error, ord_error,asc_error,minH, maxH,
-         cape,cin,pw,el,lcl,lfc, MLD, df_data_qc] =  sqf.qc_check(df_data, f_name, path_in)
-        
-        qc_path = r"D:\Astral\data\2024\Leg2\Radiosonde\netcdf\qc_report\qc_soundings\\"
-        df_data_qc.to_csv(qc_path + f_name.split('.nc')[0] + '.csv')
+        [num_error, lim_error, ord_error,minH, maxH,
+         cape,cin,pw,el,lcl,lfc, MLD] =  sqf.qc_check(df_data, f_name, path_in)
                 
         num_errors.append(num_error)
         lim_errors.append(num_error)
         ord_errors.append(num_error)
-        asc_errors.append(asc_error)
         capes.append(cape)
         cins.append(cin)
         pws.append(pw)
@@ -102,10 +92,10 @@ for f_name in file_list:
     else:
         print("Not a netCDF file:" + f_name)
         
-df_sounding_info = pd.DataFrame(list(zip(file_names,num_errors,lim_errors,ord_errors,asc_errors,
+df_sounding_info = pd.DataFrame(list(zip(file_names,num_errors,lim_errors,ord_errors,
                                          min_heights,max_heights,capes,cins,pws,els,
                                          lcls,lfcs, mix_lay_ds)), 
-                  columns = ['File', 'Numerical test', 'Limit test','Order test', 'Ascent test',
+                  columns = ['File', 'Numerical test', 'Limit test','Order test', 
                              'Min Height (hPa)','Max Height (hPa)','CPAE (J/kg)', 'CIN (J/kg)','PW (mm)',
                              'EL (hPa)','LCL (hPa)','LFC (hPa)', 'Mixed layer depth (m)'])        
         
